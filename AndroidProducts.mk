99f7b8aa5c6a6f496739ec7a75a09e23aef7ca50
Ruega <purelbatako@gmail.com>
2022-01-20 05:57:23 +0000

    X00TD: Bringup awaken-OS
    Signed-off-by: Ruega <purelbatako@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,8 +5,10 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_X00TD.mk
+    $(LOCAL_DIR)/awaken_X00TD.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_X00TD-userdebug
+    awaken_X00TD-user \
+    awaken_X00TD-userdebug \
+    awaken_X00TD-eng
 
